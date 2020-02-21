8e1ca7dc70ff6452652f4c7e934e7f635a4430a2
CraftyDragon678 <craftydragon678@gmail.com>
2020-02-21 09:24:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { Fab, List, ListItem, ListItemText, TextField } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="diag">
+        <div className="title">To-Do</div>
+        <List>
+          <ListItem button>
+            <ListItemText primary="test"/>
+          </ListItem>
+        </List>
+        <TextField
+          className="addTodo"
+          label="test"
+        />
+        <Fab>
+          <AddIcon />
+        </Fab>
+      </div>
     </div>
   );
 }
